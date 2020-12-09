38316e26b479826bc948313bb203059803a7f134
AnOrdinaryUsser <61872281+AnOrdinaryUsser@users.noreply.github.com>
2020-12-09 13:20:08 +0100

    Add files via upload

---
+++ Temperaturas/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Práctica_1_1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Boton_Transformar_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox tbc = Text_Celsius;
+            TextBox tbf = Text_Fahrenheit;
+            TextBox tf = Text_Fah;
+            TextBox tc = Text_Cel;
+
+            double celsius, fahrenheit, C, F;
+
+            C = double.Parse(tbc.Text);
+            F = double.Parse(tbf.Text);
+
+            celsius = (F - 32) * (5.0/9.0);
+            fahrenheit = (C * (9.0/5.0)) +32;
+
+            tf.Text = fahrenheit.ToString();
+            tc.Text = celsius.ToString();
+        }
+    }
+}
